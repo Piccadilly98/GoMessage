92e081ec6725ea2ed542a35a6fac553c3e73af93
Piccadilly98 <senior.filippov2017@yandex.ru>
2026-01-20 16:32:29 +0400

    add constructor service

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"github.com/Piccadilly98/GoMessage/internal/repository"
+)
+
+type Service struct {
+	userRepo    repository.UserRepository
+	chatRepo    repository.ChatRepository
+	messageRepo repository.MessageRepository
+	cache       repository.CacheRepository
+}
+
+func NewService(
+	userRepo repository.UserRepository,
+	chatRepo repository.ChatRepository,
+	messageRepo repository.MessageRepository,
+	cache repository.CacheRepository,
+) *Service {
+	return &Service{
+		userRepo:    userRepo,
+		chatRepo:    chatRepo,
+		cache:       cache,
+		messageRepo: messageRepo,
+	}
+}
--- internal/transport/http/dto/request/create_user.go
+++ internal/transport/http/dto/request/create_user.go
@@ -1,1 +1,18 @@
 package request
+
+import "fmt"
+
+type RegistrationUserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"pasword"`
+}
+
+func (ru *RegistrationUserRequest) Validate() error {
+	if ru.Username == "" {
+		return fmt.Errorf("invalid username: empty")
+	}
+	if ru.Password == "" {
+		return fmt.Errorf("invalid password: empty")
+	}
+	return nil
+}
